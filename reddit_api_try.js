a2d4aa460daffb23f0ee2462ea503642cfba06af
ffaedric <fabian_jakob.faedric@smail.th-koeln.de>
2022-01-26 14:28:27 +0100

    Grundprinzip Game

--- reddit_api_try.js
+++ reddit_api_try.js
@@ -4,14 +4,13 @@ let alphaUps = 100;
 let alphaWrong = 0;
 let buttonAnswer1;
 let buttonAnswer2;
-let buttonNext;
-let nummertest = 0;
+
 let midX;
 let midY;
-let button1Pressed = false;
-let button2Pressed = false;
-let ersterRichtig;
 
+let index = 0;
+let score = 0;
+let richtigoderfalsch = "Viel Glück beim Raten";
 
 function preload(){
     redditJson = loadJSON('https://www.reddit.com/r/AMA/.json');
@@ -44,91 +43,71 @@ function setup(){
 }
 
 function draw(){
-
-
     background(0);
 
     fill(100, 100, 100, 100);
     text("Which Post has more likes?", midX, midY - 100);
+//score
+    fill(100, 100, 100, 100);
+    text("Score: " + score, midX + 250, midY - 100);
 
-    gameMechanic()
+//Richtig oder Falsch
+    fill(100, 100, 100, 100);
+    text(richtigoderfalsch, midX, midY + 300);
+
+    fill(50, 50, 50, 100);
+    text(redditData[index].title, midX, midY);
+
+    fill(50, 50, 50, alphaUps);
+    text(redditData[index].ups, midX, midY + 10);
+
+    fill(50, 50, 50, 100)
+    text(redditData[index +1].title, midX, midY + 50);
+
+    fill(50, 50, 50, alphaUps);
+    text(redditData[index +1].ups, midX, midY + 60);
+
+
+    buttonAnswer1.mousePressed(onclickhigher)
+    buttonAnswer2.mousePressed(onclicklower)
+
+
+
+
+    // fill(50, 50, 50, alphaWrong);
+    // text("This is wrong", midX, midY - 50);
 
 
 }
 
-function gameMechanic(){
+function onclickhigher() {
+    if(redditData[index].ups > redditData[index + 1].ups || redditData[index].ups == redditData[index + 1].ups ){
+        //"This is Right"
+        richtigoderfalsch = "Das war Richtig der post hat " + (redditData[index].ups - redditData[index +1].ups) + " upvotes mehr "
+        score++
+        index++
 
-
-    fill(50, 50, 50, 100);
-    text(redditData[nummertest].title, midX, midY);
-
-    fill(50, 50, 50, alphaUps);
-    text(redditData[nummertest].ups, midX, midY + 10);
-
-    fill(50, 50, 50, 100)
-    text(redditData[nummertest + 1].title, midX, midY + 50);
-
-    fill(50, 50, 50, alphaUps);
-    text(redditData[nummertest +1].ups, midX, midY + 60);
-
-
-
-
-    buttonAnswer1.mousePressed(button1Pressed = true);
-
-    buttonAnswer2.mousePressed(button2Pressed = true);
-
-
-    if(redditData[nummertest].ups > redditData[nummertest +1].ups){
-        ersterRichtig = true
-    }else {
-        ersterRichtig = false
+    }else{
+        richtigoderfalsch = "Das war Falsch der post hat " + (redditData[index + 1].ups - redditData[index -1].ups) + " upvotes weniger "
+        //"This is wrong"
+        score = 0
+        index++
     }
 
+}
 
-    if(ersterRichtig == true && button1Pressed == true){
-        fill(50, 50, 50, alphaWrong);
-        text("This is Richtig", midX, midY - 50);
-        button1Pressed = false;
-        button2Pressed = false;
-        ersterRichtig = undefined;
-        ersterRichtig = undefined;
-        buttonNext()
+function onclicklower() {
+    if(redditData[index+ 1].ups > redditData[index].ups || redditData[index + 1].ups == redditData[index ].ups ){
+        richtigoderfalsch = "Das war Richtig der post hat " + (redditData[index + 1].ups - redditData[index].ups) + " upvotes mehr "
+        //"This is Right"
+        score++
+        index++
 
-
-    }else if(ersterRichtig == false && button2Pressed == true){
-        fill(50, 50, 50, alphaWrong);
-        text("This is Richtig", midX, midY - 50);
-        button1Pressed = false;
-        button2Pressed = false;
-        ersterRichtig = undefined;
-        buttonNext()
-
+    }else{
+        //"This is wrong"
+        richtigoderfalsch = "Das war Falsch der post hat " + (redditData[index].ups - redditData[index +1].ups) + " upvotes weniger "
+        score = 0
+        index++
     }
-    else if(button1Pressed == true || button2Pressed == true && ersterRichtig != false && button2Pressed != true || ersterRichtig != true && button1Pressed != true){
-        fill(50, 50, 50, alphaWrong);
-        text("This is wrong", midX, midY - 50);
-        button1Pressed = false;
-        button2Pressed = false;
-        ersterRichtig = undefined;
-
-        buttonNext()
-
-    }
-
-    function buttonNext(){
-
-        buttonnext = createButton('Next');
-        buttonnext.position(midX - 250, midY + 100);
-
-
-
-
-    }
-
-
-
-
-
 
 }
